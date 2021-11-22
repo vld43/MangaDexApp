52bc85a865baa78b5980193da2fed4bb8b687abe
vova4 <vld43>
2021-11-23 00:29:31 +0300

    Создание MangaRepository.

--- app/src/main/java/ru/vld/mangadexapp/data/response/MangaResponse.kt
+++ app/src/main/java/ru/vld/mangadexapp/data/response/MangaResponse.kt
@@ -1,9 +1,19 @@
 package ru.vld.mangadexapp.data.response
 
 import com.google.gson.annotations.SerializedName
+import ru.vld.mangadexapp.domain.Manga
+
+private const val RELATIONSHIP_TYPE = "cover_art"
 
 data class MangaResponse(
     @SerializedName("id") val id: String,
     @SerializedName("attributes") val attributes: MangaAttributesResponse,
     @SerializedName("relationships") val relationships: List<RelationshipResponse>
-)
+) {
+    fun transform() = Manga(
+        id = id,
+        title = attributes.title.en,
+        description = attributes.description.en,
+        coverId = relationships.filter { it.type == RELATIONSHIP_TYPE }.first().id
+    )
+}
