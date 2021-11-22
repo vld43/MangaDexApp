af021116a879e7887ad4e2761f45716d996ccc37
vova4 <vld43>
2021-11-23 00:31:30 +0300

    Refactoring.

--- app/src/main/java/ru/vld/mangadexapp/data/response/MangaResponse.kt
+++ app/src/main/java/ru/vld/mangadexapp/data/response/MangaResponse.kt
@@ -3,13 +3,16 @@ package ru.vld.mangadexapp.data.response
 import com.google.gson.annotations.SerializedName
 import ru.vld.mangadexapp.domain.Manga
 
-private const val RELATIONSHIP_TYPE = "cover_art"
-
 data class MangaResponse(
     @SerializedName("id") val id: String,
     @SerializedName("attributes") val attributes: MangaAttributesResponse,
     @SerializedName("relationships") val relationships: List<RelationshipResponse>
 ) {
+
+    companion object {
+        private const val RELATIONSHIP_TYPE = "cover_art"
+    }
+
     fun transform() = Manga(
         id = id,
         title = attributes.title.en,
